c737051ec390b4cd3e35860e2ae34b45f47c4023
voxelateme <raytrace@raytrace.ru>
2024-11-08 11:20:56 +0300

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,59 @@
+﻿using mikhalevpr5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikhalevpr5
+{
+    public class Helper
+    {
+        private static telecommunicationEntities1 _context; // Static private variable to manage database context
+
+        // Method (public, so it can be accessed from any part of the program)
+        // Retrieves the database context necessary for creating a connection to the database
+        public static telecommunicationEntities1 GetContext()
+        {
+            // Checks if the connection is not established, then creates a new one
+            if (_context == null)
+            {
+                _context = new telecommunicationEntities1(); // Creates a new database connection
+            }
+            return _context; // Returns the database connection
+        }
+
+        public static bool CreateClient(string login, string passHash, string firstName, string lastName)
+        {
+            try {
+                telecommunicationEntities1 context = GetContext();
+
+                AuthInfo authInfo = new AuthInfo();
+                authInfo.login = login;
+                authInfo.PasswordHash = passHash;
+                context.AuthInfo.Add(authInfo);
+                context.SaveChanges();
+
+                long lastId = authInfo.AuthInfoID;
+
+                Clients client = new Clients();
+                client.AuthInfoID = lastId;
+                client.FirstName = firstName;
+                client.LastName = lastName;
+                client.Email = null;
+                client.Balance = 0;
+                client.RegistrationDate = DateTime.Now;
+
+                Console.WriteLine(lastId);
+                context.Clients.Add(client);
+                context.SaveChanges();
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using HashPasswords;
+using mikhalevpr5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikhalevpr5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 hasher = new Class1();
+            telecommunicationEntities1 entities = Helper.GetContext();
+
+            Console.WriteLine("Создание новой учетной записи для пользователя");
+
+            Console.Write("Введите имя пользователя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введите фамилию пользователя: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите логин пользователя: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Введите пароль пользователя: ");
+            string password = Console.ReadLine();
+
+            // Hash the password
+            string hashedPassword = hasher.hash(password);
+            Console.WriteLine("Хешированный пароль пользователя: " + hashedPassword);
+
+            bool isSuccess = Helper.CreateClient(username, hashedPassword, firstName, lastName);
+
+            if (isSuccess) {
+                Console.WriteLine("Учетная запись добавлена");
+            }
+        }
+    }
+}
